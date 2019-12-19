b34bfd797f3850831ce4bc60e155e261fb654ec5
Dung Le <dle8@u.rochester.edu>
2019-12-19 14:51:13 +0700

    Add src

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,7 @@
-package src.main.java;
+package main.java;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello, world");
     }
 }
--- src/main/java/authentication/domain/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package src.main.java.authentication.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
-
-public interface UserRepository extends JpaRepository<User, String> {}
