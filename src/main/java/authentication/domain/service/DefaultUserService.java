71be4f5e66cfa6ee0ceb00ec9bc0d47578ac553c
Dung Le <dle8@u.rochester.edu>
2019-12-20 10:54:19 +0700

    Add DefaultUserService to create new user

---
+++ src/main/java/authentication/domain/service/DefaultUserService.java
@@ -0,0 +1,31 @@
+package authentication.domain.service;
+
+import authentication.domain.model.Role;
+import authentication.domain.model.User;
+import authentication.domain.repository.RoleRepository;
+import authentication.domain.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+
+public class DefaultUserService implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    @Transactional
+    public User createUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        Role userRole = roleRepository.findByName("ROLE_USER");
+        user.addRoles(Arrays.asList(userRole));
+        return userRepository.save(user);
+    }
+}
