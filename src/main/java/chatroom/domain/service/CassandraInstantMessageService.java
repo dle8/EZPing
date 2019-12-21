9a6a05765627f913f9b50b3d01925eea367a4e6b
Dung Le <dle8@u.rochester.edu>
2019-12-21 11:57:24 +0700

    Use Cassandra to get the messages to ensure data consistency

---
+++ src/main/java/chatroom/domain/service/CassandraInstantMessageService.java
@@ -0,0 +1,40 @@
+package chatroom.domain.service;
+
+import chatroom.domain.model.ChatRoom;
+import chatroom.domain.model.InstantMessage;
+import chatroom.domain.repository.InstantMessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CassandraInstantMessageService implements InstantMessageService {
+
+    @Autowired
+    private InstantMessageRepository instantMessageRepository;
+
+    @Autowired
+    private ChatRoomService chatRoomService;
+
+    @Override
+    public void appendInstantMessageToConversations(InstantMessage instantMessage) {
+        if (instantMessage.isFromAdmin() || instantMessage.isPublic()) {
+            ChatRoom chatRoom = chatRoomService.findById(instantMessage.getChatRoomId());
+            chatRoom.getConnectedUsers().forEach(connectedUser -> {
+                instantMessage.setUsername(connectedUser.getUsername());
+                instantMessageRepository.save(instantMessage);
+            });
+        } else {
+            instantMessage.setUsername(instantMessage.getFromUser());
+            instantMessageRepository.save(instantMessage);
+            instantMessage.setUsername(instantMessage.getToUser());
+            instantMessageRepository.save(instantMessage);
+        }
+    }
+
+    @Override
+    public List<InstantMessage> findAllInstantMessagesFor(String username, String chatRoomId) {
+        return instantMessageRepository.findInstantMessageByUsernameAndChatRoomId(username, chatRoomId);
+    }
+}
