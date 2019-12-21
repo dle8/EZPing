c38657dcdf43a52b395dd49006d20dc7f9756836
Dung Le <dle8@u.rochester.edu>
2019-12-21 14:08:41 +0700

    Add comments for appending message to users' messages panel

--- src/main/java/chatroom/domain/service/CassandraInstantMessageService.java
+++ src/main/java/chatroom/domain/service/CassandraInstantMessageService.java
@@ -19,13 +19,16 @@ public class CassandraInstantMessageService implements InstantMessageService {
 
     @Override
     public void appendInstantMessageToConversations(InstantMessage instantMessage) {
+        // If the message is public or from admin
         if (instantMessage.isFromAdmin() || instantMessage.isPublic()) {
             ChatRoom chatRoom = chatRoomService.findById(instantMessage.getChatRoomId());
+            // Append the message for each connected user in the chat room
             chatRoom.getConnectedUsers().forEach(connectedUser -> {
                 instantMessage.setUsername(connectedUser.getUsername());
                 instantMessageRepository.save(instantMessage);
             });
         } else {
+            // Append the message for only sender and receiver
             instantMessage.setUsername(instantMessage.getFromUser());
             instantMessageRepository.save(instantMessage);
             instantMessage.setUsername(instantMessage.getToUser());
