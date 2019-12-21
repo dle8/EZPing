6527c5c14173f5a368ca8fe84925fd560f815f48
Dung Le <dle8@u.rochester.edu>
2019-12-21 14:37:16 +0700

    Internationalization by Spring MVC using LocaleResolver

---
+++ src/main/java/configuration/WebConfig.java
@@ -0,0 +1,31 @@
+package configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver(); // Keep locale attr in user's HTTP session
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        // Look for the "lang" parameter in the GET request, and set user's locale to "lang" parameter value.
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
