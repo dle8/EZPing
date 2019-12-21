36aa505a5890a122baadbccf0100db7750a628e4
Dung Le <dle8@u.rochester.edu>
2019-12-21 11:34:17 +0700

    Use updateConnectedUsersViaWebsocket to update online user when user joins/leave the room

---
+++ src/main/java/chatroom/domain/service/RedisChatRoomService.java
@@ -0,0 +1,71 @@
+package chatroom.domain.service;
+
+import chatroom.domain.model.ChatRoom;
+import chatroom.domain.model.ChatRoomUser;
+import chatroom.domain.model.InstantMessage;
+import chatroom.domain.repository.ChatRoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+import utils.Destinations;
+import utils.SystemMessages;
+
+import java.util.List;
+
+@Service
+public class RedisChatRoomService implements ChatRoomService {
+
+    @Autowired
+    private SimpMessagingTemplate webSocketMessagingTemplate;
+
+    @Autowired
+    private ChatRoomRepository chatRoomRepository;
+
+    @Autowired
+    private InstantMessageService instantMessageService;
+
+    @Override
+    public ChatRoom save(ChatRoom chatRoom) {
+        return chatRoomRepository.save(chatRoom);
+    }
+
+    @Override
+    public ChatRoom findById(String chatRoomId) {
+        return chatRoomRepository.findOne(chatRoomId);
+    }
+
+    private void updateConnectedUsersViaWebsocket(ChatRoom chatRoom) {
+        webSocketMessagingTemplate.convertAndSend(
+                Destinations.ChatRoom.connectedUsers(chatRoom.getId()),
+                chatRoom.getConnectedUsers()
+        );
+    }
+
+    @Override
+    public ChatRoom join(ChatRoomUser joiningUser, ChatRoom chatRoom) {
+//        chatRoom.addUser(joiningUser);
+//        chatRoomRepository.save(chatRoom);
+//        sendPublicMessage(SystemMessages.welcome(chatRoom.getId(), joiningUser.getUsername()));
+            return null;
+    }
+
+    @Override
+    public ChatRoom leave(ChatRoomUser leavingUser, ChatRoom chatRoom) {
+        return null;
+    }
+
+    @Override
+    public void sendPublicMessage(InstantMessage instantMessage) {
+
+    }
+
+    @Override
+    public void sendPrivateMessage(InstantMessage instantMessage) {
+
+    }
+
+    @Override
+    public List<ChatRoom> findAll() {
+        return null;
+    }
+}
