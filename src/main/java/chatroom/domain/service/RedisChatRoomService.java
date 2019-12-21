07091252e21ba1052a63aed475fa45e5464e20db
Dung Le <dle8@u.rochester.edu>
2019-12-21 14:09:45 +0700

    Add comments to clarify message flow for sending private/ public messages

--- src/main/java/chatroom/domain/service/RedisChatRoomService.java
+++ src/main/java/chatroom/domain/service/RedisChatRoomService.java
@@ -41,37 +41,20 @@ public class RedisChatRoomService implements ChatRoomService {
         );
     }
 
-    // join a provided user to a provided chatroom in the parameters
-    @Override
-    public ChatRoom join(ChatRoomUser joiningUser, ChatRoom chatRoom) {
-        chatRoom.addUser(joiningUser);
-        chatRoomRepository.save(chatRoom);
-        sendPublicMessage(SystemMessages.welcome(chatRoom.getId(), joiningUser.getUsername()));
-        updateConnectedUsersViaWebsocket(chatRoom);
-        return chatRoom;
-    }
-
-    // leave a provided user from a provided chatroom in the parameters
-    @Override
-    public ChatRoom leave(ChatRoomUser leavingUser, ChatRoom chatRoom) {
-        sendPublicMessage(SystemMessages.goodbye(chatRoom.getId(), leavingUser.getUsername()));
-        chatRoom.removeUser(leavingUser);
-        chatRoomRepository.save(chatRoom);
-        updateConnectedUsersViaWebsocket(chatRoom);
-        return chatRoom;
-    }
-
     @Override
     public void sendPublicMessage(InstantMessage instantMessage) {
+        // Send message to destination: "/topic/" + chatRoomId + ".public.messages";
         webSocketMessagingTemplate.convertAndSend(
                 Destinations.ChatRoom.publicMessages(instantMessage.getChatRoomId()),
                 instantMessage
         );
+        // Store message to user' conversations in Cassandra
         instantMessageService.appendInstantMessageToConversations(instantMessage);
     }
 
     @Override
     public void sendPrivateMessage(InstantMessage instantMessage) {
+        // Send message to "/queue/" + chatRoomId + ".private.messages";
         webSocketMessagingTemplate.convertAndSendToUser(
                 instantMessage.getToUser(),
                 Destinations.ChatRoom.privateMessages(instantMessage.getChatRoomId()),
@@ -82,7 +65,29 @@ public class RedisChatRoomService implements ChatRoomService {
                 Destinations.ChatRoom.privateMessages(instantMessage.getChatRoomId()),
                 instantMessage
         );
+        // Store message to user' conversations in Cassandra
         instantMessageService.appendInstantMessageToConversations(instantMessage);
+    }
+
+    // Join a provided user to a provided chatroom in the parameters
+    @Override
+    public ChatRoom join(ChatRoomUser joiningUser, ChatRoom chatRoom) {
+        chatRoom.addUser(joiningUser);
+        chatRoomRepository.save(chatRoom);
+        sendPublicMessage(SystemMessages.welcome(chatRoom.getId(), joiningUser.getUsername()));
+        // Send a public message to "/topic/" + chatRoomId + ".connected.users" destination to update connected users.
+        updateConnectedUsersViaWebsocket(chatRoom);
+        return chatRoom;
+    }
+
+    // Leave a provided user from a provided chatroom in the parameters. Same flow as joining
+    @Override
+    public ChatRoom leave(ChatRoomUser leavingUser, ChatRoom chatRoom) {
+        sendPublicMessage(SystemMessages.goodbye(chatRoom.getId(), leavingUser.getUsername()));
+        chatRoom.removeUser(leavingUser);
+        chatRoomRepository.save(chatRoom);
+        updateConnectedUsersViaWebsocket(chatRoom);
+        return chatRoom;
     }
 
     @Override
